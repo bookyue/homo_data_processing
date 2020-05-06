5d09a7cc78a2136e926f347031cce431a3207959
bookyue <sevenbookyue@gmail.com>
2020-05-06 16:53:09 +0800

    add organize_files function in convert_out_to_csv.py

--- convert_out_to_csv.py
+++ convert_out_to_csv.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import re
 import glob
-
+from pathlib import Path
+import shutil
 
 # convert out file to csv file
 def out_to_csv(file_name):
-    read_file = pd.read_fwf(file_name)
-    out_csv_file = pd.DataFrame(read_file, columns=["NucID", "nuc_name", "Unnamed: 3"])
-    out_csv_file.rename(columns={"Unnamed: 3": "result"}, inplace=True)
+    read_file = pd.read_csv(file_name, sep="\s+", skiprows=1, names=["nucid", "nuc_name", "remove", "result"])
+
+    out_csv_file = pd.DataFrame(read_file, columns=["nucid", "nuc_name", "result"])
 
     pattern = re.compile("^(.*)\.out$")
     base_file_name =  pattern.match(file_name).group(1)
@@ -16,11 +17,21 @@ def out_to_csv(file_name):
     out_csv_file.to_csv(out_file_name, index=0)
 
 
+def organize_files():
+    Path("./csv").mkdir(exist_ok=True)
+
+    file_names = glob.glob("./*.csv")
+    for file_name in file_names:
+        shutil.move(file_name, "./csv")
+    return
+
 def main():
     file_names = glob.glob("./*.out")
     for file_name in file_names:
         print(file_name)
         out_to_csv(file_name)
+
+    organize_files()
 
 
 main()
