c1df26d0872c8e4896907b4efc18b528804541cc
bookyue <sevenbookyue@gmail.com>
2020-05-11 12:34:06 +0800

    add handle gamma function and change to formal output file name

--- convert_out_to_csv.py
+++ convert_out_to_csv.py
@@ -4,14 +4,20 @@ import glob
 from pathlib import Path
 import shutil
 
-# convert out file to csv file
-def out_to_csv(file_name):
-    read_file = pd.read_csv(file_name, sep="\s+", skiprows=1, names=["nucid", "nuc_name", "remove", "result"])
 
-    out_csv_file = pd.DataFrame(read_file, columns=["nucid", "nuc_name", "result"])
+# convert out file to csv file
+def out_to_csv(file_name, is_gamma):
+    if is_gamma:
+        read_file = pd.read_csv(file_name, sep="\s+", skiprows=1, names=["Energy", "remove", "result"],
+                                skipfooter=2, dtype={'Energy': str})
+        out_csv_file = pd.DataFrame(read_file, columns=["Energy", "result"])
+    else:
+        read_file = pd.read_csv(file_name, sep="\s+", skiprows=1, names=["nucid", "nuc_name", "remove", "result"])
+        out_csv_file = pd.DataFrame(read_file, columns=["nucid", "nuc_name", "result"])
+
 
     pattern = re.compile("^(.*)\.out$")
-    base_file_name =  pattern.match(file_name).group(1)
+    base_file_name = pattern.match(file_name).group(1)
     out_file_name = base_file_name + ".csv"
 
     out_csv_file.to_csv(out_file_name, index=0)
@@ -25,11 +31,15 @@ def organize_files():
         shutil.move(file_name, "./csv")
     return
 
+
 def main():
     file_names = glob.glob("./*.out")
     for file_name in file_names:
         print(file_name)
-        out_to_csv(file_name)
+        if 'gamma' in file_name:
+            out_to_csv(file_name, True)
+        else:
+            out_to_csv(file_name)
 
     organize_files()
 
--- data_processing.py
+++ data_processing.py
@@ -1,37 +1,36 @@
+import os
+import re
 import pandas as pd
 import numpy as np
 import glob
 from collections import deque
 
 
-def method_compare(in_file1, in_file2):
-    df1 = pd.read_csv(in_file1)
-    df2 = pd.read_csv(in_file2)
+def method_compare(in_file1, in_file2, is_gamma):
+    df1 = pd.read_csv(in_file1, dtype={'Energy': str})
+    df2 = pd.read_csv(in_file2, dtype={'Energy': str})
 
-    df_merged = pd.merge(df1, df2, how="outer", on=["nucid", "nuc_name"])
-    df_merged.replace(to_replace=[np.inf, -np.inf], value=np.nan, inplace=True)
+    if is_gamma:
+        df_merged = pd.merge(df1, df2, how="outer", on=['Energy'])
+        df_merged.replace(to_replace=[np.inf, -np.inf], value=np.nan, inplace=True)
+    else:
+        df_merged = pd.merge(df1, df2, how="outer", on=["nucid", "nuc_name"])
+        df_merged.replace(to_replace=[np.inf, -np.inf], value=np.nan, inplace=True)
+
     print(df_merged)
 
     # relative tolerance
     # abs(X - Y) / 1 + min(abs(X), abs(Y))
-    relative_numeric_tolerance_e_5 = (np.abs(df_merged.iloc[:, 2] - df_merged.iloc[:, 3]) / (
-                1 + np.where(np.abs(df_merged.iloc[:, 2]) < np.abs(df_merged.iloc[:, 3]), np.abs(df_merged.iloc[:, 2]),
-                             np.abs(df_merged.iloc[:, 3])))) > 1e-5
+    relative_numeric_tolerance_e_5 = (np.abs(df_merged.iloc[:, -2] - df_merged.iloc[:, -1]) / (
+            1 + np.where(np.abs(df_merged.iloc[:, -2]) < np.abs(df_merged.iloc[:, -1]), np.abs(df_merged.iloc[:, -2]),
+                         np.abs(df_merged.iloc[:, -1])))) > 1e-5
 
-    is_not_the_same = df_merged.iloc[:, 2] != df_merged.iloc[:, 3]
-    is_too_small_number_x = np.abs(df_merged.iloc[:, 2]) > 1e-30
-    is_too_small_number_y = np.abs(df_merged.iloc[:, 3]) > 1e-30
+    is_not_the_same = df_merged.iloc[:, -2] != df_merged.iloc[:, -1]
+    is_too_small_number_x = np.abs(df_merged.iloc[:, -2]) > 1e-30
+    is_too_small_number_y = np.abs(df_merged.iloc[:, -1]) > 1e-30
 
-    nuclide_list = ['Np237', 'Pa233', 'U233', 'Th229', 'Ra225', 'Ac225', 'Fr221', 'At217', 'Bi213', 'Po213', 'Tl209',
-                    'Pb209', 'Bi209', 'U234', 'U235', 'U236', 'U238', 'U239', 'Np237', 'Np239', 'Pu238', 'Pu239',
-                    'Pu240', 'Pu241', 'Pu242', 'Am241', 'Am242', 'Am243', 'Cm242', 'Cm244', 'H3', 'Mo95', 'Tc99',
-                    'Ru103', 'Ag109', 'Xe135', 'Cs133', 'Nd143', 'Nd145', 'Sm147', 'Sm149', 'Sm150', 'Sm151', 'Sm152',
-                    'Eu153', 'Gd155']
-
-    # df_merged = df_merged[df_merged['nuc_name'].isin(nuclide_list)]
-    df_merged = pd.DataFrame(
-        df_merged[is_not_the_same & relative_numeric_tolerance_e_5 & is_too_small_number_x & is_too_small_number_y])
-    df_output = df_merged[df_merged['nuc_name'].isin(nuclide_list)]
+    df_output = pd.DataFrame(df_merged[is_not_the_same & relative_numeric_tolerance_e_5 &
+                                       is_too_small_number_x & is_too_small_number_y])
 
     return df_output
 
@@ -39,32 +38,72 @@ def method_compare(in_file1, in_file2):
 def group_compare():
     df_add = deque()
     file_names = glob.glob("./*.csv")
+
+    pattern = re.compile("^\./(\D+)\d+-\d+-(\w+)\.csv$")
+    front_base_file_name = pattern.match(file_names[0]).group(1)
+    end_base_file_name = pattern.match(file_names[0]).group(2)
+
+    if 'gamma' == end_base_file_name:
+        is_gamma = True
+    else:
+        is_gamma = False
+
     i = 0
     while True:
         for j in range(i + 1, i + 5):
-            df_add.append(method_compare(file_names[i], file_names[j]))
+            df_add.append(method_compare(file_names[i], file_names[j], is_gamma))
             # print(df_add.pop())
-        df_result = df_add.popleft()
-        df_result.columns = ["nucid", "nuc_name", "TTA", "CRAM"]
+        if is_gamma:
+            df_result = df_add.popleft()
+            df_result.columns = ['Energy', "TTA", "CRAM"]
 
-        tmp = df_add.popleft()
-        tmp.columns = ["nucid", "nuc_name", "TTA", "QRAM"]
-        df_result = pd.merge(df_result, tmp, how="outer", on=["nucid", "nuc_name", "TTA"])
+            tmp = df_add.popleft()
+            tmp.columns = ['Energy', "TTA", "QRAM"]
+            df_result = pd.merge(df_result, tmp, how="outer", on=['Energy', "TTA"])
 
-        tmp = df_add.popleft()
-        tmp.columns = ["nucid", "nuc_name", "TTA", "LPAM"]
-        df_result = pd.merge(df_result, tmp, how="outer", on=["nucid", "nuc_name", "TTA"])
+            tmp = df_add.popleft()
+            tmp.columns = ['Energy', "TTA", "LPAM"]
+            df_result = pd.merge(df_result, tmp, how="outer", on=['Energy', "TTA"])
 
-        tmp = df_add.popleft()
-        tmp.columns = ["nucid", "nuc_name", "TTA", "MMPA"]
-        df_result = pd.merge(df_result, tmp, how="outer", on=["nucid", "nuc_name", "TTA"])
+            tmp = df_add.popleft()
+            tmp.columns = ['Energy', "TTA", "MMPA"]
+            df_result = pd.merge(df_result, tmp, how="outer", on=['Energy', "TTA"])
 
-        # correct colums order
-        df_result = df_result[["nucid", "nuc_name", "TTA", "CRAM", "QRAM", "LPAM", "MMPA"]]
+            df_result = df_result[['Energy', "TTA", "CRAM", "QRAM", "LPAM", "MMPA"]]
 
-        df_result.sort_values(by='nucid', inplace=True)
+        else:
+            df_result = df_add.popleft()
+            df_result.columns = ["nucid", "nuc_name", "TTA", "CRAM"]
+
+            tmp = df_add.popleft()
+            tmp.columns = ["nucid", "nuc_name", "TTA", "QRAM"]
+            df_result = pd.merge(df_result, tmp, how="outer", on=["nucid", "nuc_name", "TTA"])
+
+            tmp = df_add.popleft()
+            tmp.columns = ["nucid", "nuc_name", "TTA", "LPAM"]
+            df_result = pd.merge(df_result, tmp, how="outer", on=["nucid", "nuc_name", "TTA"])
+
+            tmp = df_add.popleft()
+            tmp.columns = ["nucid", "nuc_name", "TTA", "MMPA"]
+            df_result = pd.merge(df_result, tmp, how="outer", on=["nucid", "nuc_name", "TTA"])
+
+            # correct columns order
+            df_result = df_result[["nucid", "nuc_name", "TTA", "CRAM", "QRAM", "LPAM", "MMPA"]]
+
+            df_result.sort_values(by='nucid', inplace=True)
+
         print(df_result)
-        df_result.to_csv("结果" + str(i * 6) + ".csv", index=0)
+        outname = f'{front_base_file_name}{str((i * 6) + 1).zfill(3)}-' \
+                  f'{str((i + 5) * 6).zfill(3)}-{end_base_file_name}.csv '
+
+        outdir = './result'
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+
+        fullname = os.path.join(outdir, outname)
+
+        df_result.to_csv(fullname, index=0)
+
         i += 5
 
         if i == len(file_names):
