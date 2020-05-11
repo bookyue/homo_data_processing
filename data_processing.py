d5807ea5132e8c46b1cb21705cdc080589aec9fd
bookyue <sevenbookyue@gmail.com>
2020-05-11 18:28:01 +0800

    fix nuclide_list filter

--- data_processing.py
+++ data_processing.py
@@ -16,6 +16,11 @@ def method_compare(in_file1, in_file2, is_gamma):
     else:
         df_merged = pd.merge(df1, df2, how='outer', on=['nucid', 'nuc_name'])
         df_merged.replace(to_replace=[np.inf, -np.inf], value=np.nan, inplace=True)
+        nuclide_list = ['Np237', 'Pa233', 'U233', 'Th229', 'Ra225', 'Ac225', 'Fr221', 'At217', 'Bi213', 'Po213',
+                        'Tl209', 'Pb209', 'Bi209', 'U234', 'U235', 'U236', 'U238', 'U239', 'Np237', 'Np239', 'Pu238',
+                        'Pu239', 'Pu240', 'Pu241', 'Pu242', 'Am241', 'Am242', 'Am243', 'Cm242', 'Cm244', 'H3', 'Mo95',
+                        'Tc99', 'Ru103', 'Ag109', 'Xe135', 'Cs133', 'Nd143', 'Nd145', 'Sm147', 'Sm149', 'Sm150',
+                        'Sm151', 'Sm152', 'Eu153', 'Gd155']
 
     print(df_merged)
 
@@ -31,7 +36,8 @@ def method_compare(in_file1, in_file2, is_gamma):
 
     df_output = pd.DataFrame(df_merged[is_not_the_same & relative_numeric_tolerance_e_5 &
                                        is_too_small_number_x & is_too_small_number_y])
-
+    if not is_gamma:
+        df_output = df_output[df_output['nuc_name'].isin(nuclide_list)]
     return df_output
 
 
