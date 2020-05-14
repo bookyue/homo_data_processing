c9e9631dff8a6bd5baa708e5eec6ff88ad333fd5
bookyue <sevenbookyue@gmail.com>
2020-05-14 10:04:36 +0800

    update convert_out_to_csv.py skip 3 footers for normal out file

--- convert_out_to_csv.py
+++ convert_out_to_csv.py
@@ -8,18 +8,19 @@ import shutil
 # convert out file to csv file
 def out_to_csv(file_name, is_gamma=False):
     if is_gamma:
-        read_file = pd.read_csv(file_name, sep='\s+', skiprows=1, names=['Energy', 'remove', 'result'],
-                                skipfooter=2, dtype={'Energy': str})
+        read_file = pd.read_csv(file_name, sep='\s+', skiprows=1, skipfooter=2, engine='python',
+                                names=['Energy', 'remove', 'result'], dtype={'Energy': str})
         out_csv_file = pd.DataFrame(read_file, columns=['Energy', 'result'])
     else:
-        read_file = pd.read_csv(file_name, sep='\s+', skiprows=1, names=['nucid', 'nuc_name', 'remove', 'result'])
+        read_file = pd.read_csv(file_name, sep='\s+', skiprows=1, skipfooter=3, engine='python',
+                                names=['nucid', 'nuc_name', 'remove', 'result'])
         out_csv_file = pd.DataFrame(read_file, columns=['nucid', 'nuc_name', 'result'])
 
-    pattern = re.compile('^(.*)\.out$')
+    pattern = re.compile('^(.*)\\.out$')
     base_file_name = pattern.match(file_name).group(1)
     out_file_name = base_file_name + '.csv'
 
-    out_csv_file.to_csv(out_file_name, index=0)
+    out_csv_file.to_csv(out_file_name, index=False)
 
 
 def organize_files():
